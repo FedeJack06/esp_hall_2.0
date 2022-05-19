56328f0ab92d6eae8260b28d30e089b7ba13f8e0
andreacosso <andrebog11@gmail.com>
2022-05-19 15:00:55 +0200

    agag

--- caratterizzazione/calcoli_incertezze/incertezze_massime.py
+++ caratterizzazione/calcoli_incertezze/incertezze_massime.py
@@ -1,17 +1,15 @@
-import uncertanites 
+import uncertainties
 from uncertainties import ufloat
 
 
 
-
-
-R_A  = ufloat( 217 ,  )
-R_B1 = ufloat( 2150 ,  ) 
-R_B2 = ufloat( 2160 ,  ) 
-R_C1 = ufloat( 558 ,  ) 
-R_C2 = ufloat( 557 ,  ) 
-R_D1 = ufloat( 5470 ,  ) 
-R_D2 = ufloat( 5480 ,  ) 
+R_A  = ufloat( 217 , 6  )
+R_B1 = ufloat( 2150 , 26 ) 
+R_B2 = ufloat( 2160 , 26 ) 
+R_C1 = ufloat( 558 , 10 ) 
+R_C2 = ufloat( 557 , 10 ) 
+R_D1 = ufloat( 5470 , 59 ) 
+R_D2 = ufloat( 5480 , 59 ) 
 
 
 G_diff = 0.5*((1+(R_D2)/(R_C2))*(R_D1)/(R_C1+R_D1)+(R_D2)/(R_C2))*(1+2*(R_B1)/(R_A))
